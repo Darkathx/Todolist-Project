822974696a2e82c57518c2bfa9241ce1319adcf2
Meriç Kerem Yalçın <merickereme@gmail.com>
2022-09-09 20:37:00 +0300

    initial html-css code for todolist project

---
+++ app.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("#todo-form");
+const todoInp = document.querySelector("#todoinp");
+const todoList = document.querySelector(".todo-list");
+const todoBody = document.querySelector(".todo-body");
+const todoSearch = document.querySelector(".todosearch");
